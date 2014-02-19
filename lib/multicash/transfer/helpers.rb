f09f25bb0da9331b6b5dfab5009c31f53ae9e579
Anton Yordanov <anton@empowerunited.com>
2014-02-19 09:16:52 +0200

    fix namespaces

--- lib/multicash/transfer/helpers.rb
+++ lib/multicash/transfer/helpers.rb
@@ -1,55 +1,53 @@
 module Multicash
-  module Transfer
-    module Helpers
+  module Transfer::Helpers
 
-      def ammount
-        self.payment.ammount_value.to_s
-      end
-
-      def currency
-        self.payment.ammount_currency
-      end
-
-      def destination_bank_name
-        self.destination_account.bank_name ||= "DESTINATION BANK NAME"
-      end
-
-      def destination_name
-        self.destination_account.name
-      end
-
-      def destination_iban
-        self.destination_account.iban
-      end
-
-      def destination_bae
-        self.destination_account.bae
-      end
-
-      def ordering_bank_name
-        self.ordering_account.bank_name ||= "ORDERING BANK NAME"
-      end
-
-      def ordering_name
-        self.ordering_account.name
-      end
-
-      def ordering_bae
-        self.ordering_account.bae
-      end
-
-      def ordering_iban
-        self.ordering_account.iban
-      end
-
-      def payment_details
-        self.payment.details
-      end
-
-      def payment_charges
-        self.payment.charges
-      end
-
+    def ammount
+      self.payment.ammount_value.to_s
     end
+
+    def currency
+      self.payment.ammount_currency
+    end
+
+    def destination_bank_name
+      self.destination_account.bank_name ||= "DESTINATION BANK NAME"
+    end
+
+    def destination_name
+      self.destination_account.name
+    end
+
+    def destination_iban
+      self.destination_account.iban
+    end
+
+    def destination_bae
+      self.destination_account.bae
+    end
+
+    def ordering_bank_name
+      self.ordering_account.bank_name ||= "ORDERING BANK NAME"
+    end
+
+    def ordering_name
+      self.ordering_account.name
+    end
+
+    def ordering_bae
+      self.ordering_account.bae
+    end
+
+    def ordering_iban
+      self.ordering_account.iban
+    end
+
+    def payment_details
+      self.payment.details
+    end
+
+    def payment_charges
+      self.payment.charges
+    end
+
   end
 end
--- lib/multicash/transfer/labels_builder.rb
+++ lib/multicash/transfer/labels_builder.rb
@@ -1,14 +1,10 @@
 module Multicash
-  module Transfer
-    class LabelsBuilder
-
-      def self.build transfer
-        case transfer
-        when Multicash::CreditTransfer
-          Labels::CreditTransfer.new(transfer)
-        end
+  class Transfer::LabelsBuilder
+    def self.build transfer
+      case transfer
+      when Multicash::CreditTransfer
+        Labels::CreditTransfer.new(transfer)
       end
-
     end
   end
 end
